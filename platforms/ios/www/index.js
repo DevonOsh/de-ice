e0977af32fb570a1295e9e4c2dfb04fb52be49ef
Devon O'Shaughnessy <devonosh@gmail.com>
2017-04-11 21:23:00 -0500

    In process of attempting angular

--- platforms/ios/www/index.js
+++ platforms/ios/www/index.js
@@ -22,15 +22,14 @@ document.addEventListener('deviceready', function() {
   */
   var deIceApp = angular.module('deIceApp',[]);
 
-  if(deIceApp) {
-    alert("Module created");
-  }
-
   deIceApp.controller('calcCtrl', ['$scope', function($scope) {
       $scope.temps = ["30", "25-30", "20-25", "15-20", "0-15", "0"];
-      $scope.forecasts = ["Rising", "Falling"];
-      $scope.weather = ["Snow", "Freezing Rain"];
-      $scope.material = ["Wetted salt: bring", "Wetted salt:other", "Dry salt", "Sand"];
+      //$scope.forecasts = ["Rising", "Falling"];
+      //$scope.weather = ["Snow", "Freezing Rain"];
+      //$scope.material = ["Wetted salt: brine", "Wetted salt:other", "Dry salt", "Sand"];
+      $scope.area = "100 sq. ft";
   }]);
+
+  deIceApp.controller('');
   
 });
